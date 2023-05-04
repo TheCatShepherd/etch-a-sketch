6d3d8a2ca810ab8399ecc881fc70ce46117dc8b5
Yann <116846756+TheCatShepherd@users.noreply.github.com>
2023-05-04 20:27:50 +0200

    Create etch-a-sketch.js

---
+++ etch-a-sketch.js
@@ -0,0 +1,12 @@
+//Create function that makes new divs (or whatever will create a div)
+//append divs to container div
+
+let gridDiv = document.querySelector("#container");
+
+function makeSquares(num) {
+   let square = document.createElement("div");
+   square.classList.add("square") //create in css
+   //square * (num * num)
+   //loop over them to container.appendChild(square) ?
+   //put them in an array to loop over?
+}
